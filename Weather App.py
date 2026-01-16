a30a4afab18a32ad2496e0098f984e77a4ca2c3e
Muhammad Zaid <muhammadzaid2009@outlook.com>
2026-01-16 14:58:18 +0500

    Add comments and usage steps to Weather App.py
    Added comments for code explanation and usage steps.

--- Weather App.py
+++ Weather App.py
@@ -1,3 +1,11 @@
+# STEPS FOR THIS CODE 
+# 1- IMPORT MODULES
+# 1- REQUESTS, JSON, OS, WIN32COM.CLIENT
+# 2- THEN ADD THIS LINE speaker = win32com.client.Dispatch("SAPI.SpVoice")
+# 3- WIN32.COM IS FOR WINDOWS IT ONLY WORKS IN WINDOWS NOT ANY OTHER OS 
+# 4- GET API KEY FROM ANY WEATHER API KEY FOR FREE 
+
+
 import requests
 import json
 import os
@@ -9,5 +17,6 @@ r = requests.get(url)
 print(r.text)
 wdic = json.loads(r.text)
 print(wdic ["current"] ["temp_c"])
+
 
 speaker.speak(f"The current temperature in {city_temp} is {wdic ['current'] ['temp_c']} degree Celsius")
